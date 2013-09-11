4fac11b247415e462b647521c541cd7fdde87ba3
jiesongk <jiesongk@umich.edu>
2013-09-10 22:33:13 -0400

    MainActivity.java

--- WeWrite/WeWrite/src/com/example/wewrite/MainActivity.java
+++ WeWrite/WeWrite/src/com/example/wewrite/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
+    asda
     setContentView(R.layout.activity_main);
   }
 
