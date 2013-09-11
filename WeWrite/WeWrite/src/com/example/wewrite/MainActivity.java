5a6b4c67d66ec06f4e636de635c19c4c321e1a97
jiesongk <jiesongk@umich.edu>
2013-09-10 22:34:39 -0400

    Error revised

--- WeWrite/WeWrite/src/com/example/wewrite/MainActivity.java
+++ WeWrite/WeWrite/src/com/example/wewrite/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends Activity
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
-    asda
     setContentView(R.layout.activity_main);
   }
 
